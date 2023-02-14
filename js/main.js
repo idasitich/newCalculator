e6c35fe141a9c1ab3792f80c0cfa07c27f61baea
idasitich <karris.kk@gmail.com>
2023-02-14 18:01:30 +0700

    adding target value

--- js/main.js
+++ js/main.js
@@ -28,7 +28,7 @@ document.addEventListener('keydown', function(event) {
     if (event.key === '1' || event.key === '2' || event.key === '3' || event.key === '4' || event.key === '5' || event.key === '6' || event.key === '7' || event.key === '8' || event.key === '9' || event.key === '0') {
         numberHandler({target: {textContent: event.key}});
     } else if (event.key === '+' || event.key === '-' || event.key === '*' || event.key === '/') {
-        operatorHandler({target: {textContent: event.key}});
+        operatorHandler({target: {value: event.key}});
     } else if (event.key === '.' || event.key === 'Decimal') {
         decimalHandler();
     } else if (event.key === 'Backspace') {
